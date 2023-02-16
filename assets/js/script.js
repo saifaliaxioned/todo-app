f32cbe50b5bf1d72d15800e6e418a609eda65e36
saifaliaxioned <saifalis@axioned.com>
2023-02-16 19:39:12 +0530

    functions added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,71 @@
-/* Author: 
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItem = document.querySelector('.todo-items');
 
-*/
-`                <li class="todo-list">write in markdown</li>
-<li class="todo-list">code in javascript</li>
-<li class="todo-list">test in cypress</li>`
+const data = JSON.parse(localStorage.getItem('todoData'));
+let collection = data ? data : [];
+
+const newList = () => {
+  const obj = {
+    todoname: todoInput.value
+  };
+  collection.push(obj);
+  localStorage.setItem('todoData', JSON.stringify(collection));
+  createList();
+  todoInput.value = "";
+}
+
+
+const createList = () => {
+  if (collection != null) {
+    let li = '';
+    collection.forEach((list) => {
+      li += `<li class="todo-list">${list.todoname}</li>`;
+    });
+    todoItem.innerHTML = li;
+    activeList();
+    completedList();
+  }
+}
+
+console.log(collection.length);
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+todoInput.addEventListener('keyup', (e) => {
+  if (e.key == 'Enter') {
+    newList();
+  }
+});
+
+
+const completedList = () => {
+  const todoList = document.querySelectorAll('.todo-list');
+  todoList.forEach((list) => {
+    list.addEventListener('click', () => {
+      list.classList.add('completed-list');
+    });
+  })
+}
+
+const activeList = () => {
+  const todoList = document.querySelectorAll('.todo-list');
+  todoList.forEach((list) => {
+    list.addEventListener('dblclick', () => {
+      list.classList.remove('completed-list');
+      todoInput.value = list.innerText;
+      console.log('kdf');
+    });
+  })
+}
+
+// initial load
+document.load = createList();
+
+
+
+
 
 
 
