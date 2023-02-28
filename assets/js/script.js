071c62001b8479e40b9ce47eb786bece4798cec2
saifaliaxioned <saifalis@axioned.com>
2023-02-28 11:37:48 +0530

    functions seprated for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 const todoForm = document.querySelector('.todo-form'),
   todoInput = document.querySelector('.todo-input'),
   todoItem = document.querySelector('.todo-items'),
-  allBtn = document.querySelector('.all-btn'),
+  allListBtn = document.querySelector('.all-btn'),
   activeBtn = document.querySelector('.active-btn'),
   completeBtn = document.querySelector('.completed-btn'),
   clearBtn = document.querySelector('.clear-btn'),
-  items = document.querySelector('.todo-result p');
+  items = document.querySelector('.todo-result p'),
+  buttonList = document.querySelectorAll('.filter-list button');
 
 const data = JSON.parse(localStorage.getItem('todoData'));
 let collection = data ? data : [], editId = null, itemLeft;
@@ -26,8 +27,8 @@ const createList = () => {
       li += `<li class="todo-list ${list.completed == null ? '' : list.completed}">${list.todoname}</li>`;
     });
     todoItem.innerHTML = li;
-    listClickEvent('click');
-    listClickEvent('dblclick');
+    activeList();
+    completeList();
     itemsCount();
   }
 }
@@ -47,56 +48,67 @@ todoInput.addEventListener('keyup', (e) => {
   }
 });
 
-const listClickEvent = (btnEvent) => {
+const activeList = () => {
   const todoList = document.querySelectorAll('.todo-list');
   todoList.forEach((list, index) => {
-    list.addEventListener(btnEvent, () => {
-      if (btnEvent === 'click') {
-        editId = null;
-        list.classList.remove('active');
-        list.classList.add('completed');
-        collection[index].completed = 'completed';
-      } else {
-        list.classList.remove('completed');
-        todoInput.value = list.innerText;
-        editId = index;
-        collection[editId].completed = null;
-      }
+    list.addEventListener('dblclick', () => {
+      list.classList.remove('completed');
+      todoInput.value = list.innerText;
+      editId = index;
+      collection[editId].completed = null;
       localStorage.setItem('todoData', JSON.stringify(collection));
       itemsCount();
-    });
-  });
-}
+    })
+  })
+};
 
-const clickEvent = (btn, btnEvent) => {
-  btn.addEventListener(btnEvent, () => {
+const completeList = () => {
+  const todoList = document.querySelectorAll('.todo-list');
+  todoList.forEach((list, index) => {
+    list.addEventListener('click', () => {
+      editId = null;
+      list.classList.remove('active');
+      list.classList.add('completed');
+      collection[index].completed = 'completed';
+      localStorage.setItem('todoData', JSON.stringify(collection));
+      itemsCount();
+    })
+  })
+};
+
+buttonList.forEach(btn => {
+  btn.addEventListener('click', () => {
     const activeBtn = document.querySelector('.active-filter');
     activeBtn.classList.remove('active-filter');
     btn.classList.add('active-filter');
-    const todoList = document.querySelectorAll('.todo-list');
-    todoList.forEach((list) => {
-      if (btn.classList.contains('all-btn')) {
-        createList();
-      } else if (btn.classList.contains('active-btn')) {
-        if (list.classList.contains('completed')) {
-          list.classList.add('hide-list');
-        } else {
-          list.classList.remove('hide-list');
-        }
-      } else {
-        if (!list.classList.contains('completed')) {
-          list.classList.add('hide-list');
-        } else {
-          list.classList.remove('hide-list');
-        }
-      }
-    });
-  });
-};
+  })
+});
 
-clickEvent(allBtn, 'click');
-clickEvent(activeBtn, 'click');
-clickEvent(completeBtn, 'click');
+allListBtn.addEventListener('click', () => {
+  createList();
+});
+
+activeBtn.addEventListener('click', () => {
+  const todoList = document.querySelectorAll('.todo-list');
+  todoList.forEach((list) => {
+    if (list.classList.contains('completed')) {
+      list.classList.add('hide-list');
+    } else {
+      list.classList.remove('hide-list');
+    }
+  });
+});
+
+completeBtn.addEventListener('click', () => {
+  const todoList = document.querySelectorAll('.todo-list');
+  todoList.forEach((list) => {
+    if (!list.classList.contains('completed')) {
+      list.classList.add('hide-list');
+    } else {
+      list.classList.remove('hide-list');
+    }
+  })
+});
 
 clearBtn.addEventListener('click', () => {
   collection = itemLeft;
